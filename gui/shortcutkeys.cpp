7a026f6d4fd0f5dbddfc3e8c7fc06ba419ff6ac6
Robert Spiegel <r.spiegel@netconomy.net>
2018-06-16 16:04:04 +0200

    #10 Work on improving standard paths vs. command line overwrites

--- gui/shortcutkeys.cpp
+++ gui/shortcutkeys.cpp
@@ -243,7 +243,7 @@ void ShortcutKeys::loadCustomKeys(QString fileName) {
         }
         file.close();
     } else {
-        qDebug() << Q_FUNC_INFO << "Unable to open" << fileName << "for reading or file does not exist.";
+        QLOG_TRACE() << "Unable to open" << fileName << "for reading or file does not exist.";
     }
 }
 
